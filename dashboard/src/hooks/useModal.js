faa7b0aee208c381baa66b1acf0d78bd7b7d84c8
Marcelo <santosmarcelo1995@live.com>
2021-04-10 17:34:32 -0300

    Fix object assignment

--- dashboard/src/hooks/useModal.js
+++ dashboard/src/hooks/useModal.js
@@ -3,11 +3,12 @@ import bus from '../utils/bus'
 const EVENT_NAME = 'modal:toggle'
 
 export default function useModal () {
-  function open ({ payload = {} }) {
+  function open (payload = {}) {
+    console.log(payload)
     bus.emit(EVENT_NAME, { status: true, ...payload })
   }
 
-  function close ({ payload = {} }) {
+  function close (payload = {}) {
     bus.emit(EVENT_NAME, { status: false, ...payload })
   }
 
