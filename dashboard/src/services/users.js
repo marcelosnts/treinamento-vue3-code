a383f7a8df3fb410e9cb8f303b92071e0f1d0754
Marcelo <santosmarcelo1995@live.com>
2021-04-11 14:16:54 -0300

    Fix user return

--- dashboard/src/services/users.js
+++ dashboard/src/services/users.js
@@ -2,6 +2,6 @@ export default httpClient => ({
   getMe: async () => {
     const response = await httpClient.get('/users/me')
 
-    return { data: response }
+    return { data: response.data }
   }
 })
