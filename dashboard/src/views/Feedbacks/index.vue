c85056a26b17bcb5d03b8c2732b5b58414973cbe
Marcelo <santosmarcelo1995@live.com>
2021-05-02 16:20:08 -0300

    Add feedbacks page

--- dashboard/src/views/Feedbacks/index.vue
+++ dashboard/src/views/Feedbacks/index.vue
@@ -1,7 +1,52 @@
 <template>
-  <h1>Home!</h1>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <header-logged />
+  </div>
+
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">Feedbacks</h1>
+    <p class="text-lg text-center text-gray-800 font-regular">
+      All of yours received feedbacks.
+    </p>
+  </div>
+
+  <div class="flex justify-center w-full pb-20">
+    <div class="w-4/5 max-w-6xl py-6 grid grid-cols-4 gap-2">
+      <div>
+        <h1 class="text-3xl font-black text-brand-darkgray">
+          List
+        </h1>
+        <suspense>
+          <template #default>
+            <filters
+              class="mt-8 animate__animated animate__fadeIn animate__faster"
+            />
+          </template>
+          <template #fallback>
+            <filters-loading class="mt-8"/>
+          </template>
+        </suspense>
+      </div>
+      <div class="px-10 pt-20 col-span-3">
+        data
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import HeaderLogged from '../../components/HeaderLogged'
+import Filters from './Filters'
+import FiltersLoading from './FiltersLoading'
+
+export default {
+  components: {
+    HeaderLogged,
+    Filters,
+    FiltersLoading
+  },
+  setup () {
+
+  }
+}
 </script>
