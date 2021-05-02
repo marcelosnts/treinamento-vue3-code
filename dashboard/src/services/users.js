47242950312cef556143a6644cdd8f76d6ab4e8b
Marcelo <santosmarcelo1995@live.com>
2021-05-02 13:18:03 -0300

    Add generateApiKey to user services

--- dashboard/src/services/users.js
+++ dashboard/src/services/users.js
@@ -3,5 +3,12 @@ export default httpClient => ({
     const response = await httpClient.get('/users/me')
 
     return { data: response.data }
+  },
+  generateApiKey: async () => {
+    const response = await httpClient.post('/users/me/apikey')
+
+    return {
+      data: response.data
+    }
   }
 })
