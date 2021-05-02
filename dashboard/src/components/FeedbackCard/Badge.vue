d0f5f7feeaae6d87f2ea2bda43f28f1c635aeede
Marcelo <santosmarcelo1995@live.com>
2021-05-02 17:01:47 -0300

    Add feedback card badge/loading

---
+++ dashboard/src/components/FeedbackCard/Badge.vue
@@ -0,0 +1,46 @@
+<template>
+  <span
+    :class="classColor"
+    class="p-2 font-black text-white uppercase rounded-full"
+  >
+    {{label}}
+  </span>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  props: {
+    type: { type: String, required: true }
+  },
+  setup (props) {
+    const label = computed(() => {
+      if (props.type === 'ISSUE') {
+        return 'issue'
+      }
+      if (props.type === 'IDEA') {
+        return 'idea'
+      }
+
+      return 'others'
+    })
+
+    const classColor = computed(() => {
+      if (props.type === 'ISSUE') {
+        return 'bg-brand-danger'
+      }
+      if (props.type === 'IDEA') {
+        return 'bg-brand-warning'
+      }
+
+      return 'bg-brand-graydark'
+    })
+
+    return {
+      label,
+      classColor
+    }
+  }
+}
+</script>
---
+++ dashboard/src/components/FeedbackCard/Loading.vue
@@ -0,0 +1,40 @@
+<template>
+  <content-loader
+    class="flex flex-col items-center rounded"
+    width="100%"
+    height="168px"
+  >
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="32px"
+      animation-duration="2s"
+    />
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="32px"
+      animation-duration="2s"
+    />
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="32px"
+      animation-duration="2s"
+    />
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="32px"
+      animation-duration="2s"
+    />
+  </content-loader>
+</template>
+
+<script>
+import ContentLoader from '../ContentLoader'
+
+export default {
+  components: { ContentLoader }
+}
+</script>
