79f35dc24e8c21ff0367939795d190f0510652c3
Marcelo <santosmarcelo1995@live.com>
2021-05-02 12:22:31 -0300

    Update credentials page

--- dashboard/src/views/Credentials/index.vue
+++ dashboard/src/views/Credentials/index.vue
@@ -19,8 +19,15 @@
         This is your API key
       </p>
 
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
       <div
-        class="flex py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-2/3"
+        v-else
+        class="flex py-3 pl-5 pr-20 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-2/3"
       >
         <span>{{store.User.currentUser.apiKey}}</span>
         <div class="flex ml-20 mr-1">
@@ -42,8 +49,16 @@
       <p class="mt-5 text-lg text-gray-800 font-regular">
         Add this script at your site to start receiving feedbacks
       </p>
+
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
       <div
-        class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-2/3 overflow-x-scroll"
+        v-else
+        class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray justify-between items-center w-full lg:w-2/3 overflow-x-scroll"
       >
         <pre>&lt;script src="https://marcelosnts-feedbacker-widget.netlify.app?apy_key={{store.User.currentUser.apiKey}}"&gt;&lt;/script&gt;</pre>
       </div>
@@ -52,7 +67,10 @@
 </template>
 
 <script>
+import { reactive } from 'vue'
+
 import HeaderLogged from '../../components/HeaderLogged'
+import ContentLoader from '../../components/ContentLoader'
 import Icon from '../../components/Icon'
 import useStore from '../../hooks/useStore'
 import palette from '../../../palette'
@@ -60,12 +78,17 @@ import palette from '../../../palette'
 export default {
   components: {
     HeaderLogged,
+    ContentLoader,
     Icon
   },
   setup () {
     const store = useStore()
+    const state = reactive({
+      isLoading: false
+    })
 
     return {
+      state,
       store,
       brandColors: palette.brand
     }
