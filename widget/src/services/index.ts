50d8ed0436b9f1a4b037dea9cae0dd31edf5913c
Marcelo <santosmarcelo1995@live.com>
2021-05-16 11:13:00 -0300

    Initial widget commit

---
+++ widget/src/services/index.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const API_ENVS = {
+  production: '',
+  development: 'http://localhost:3000'
+}
+
+const httpClient = axios.create({
+  baseURL: API_ENVS.development
+})
+
+httpClient.interceptors.response.use((response) => {
+  return response
+}, (error) => {
+  const canThrowAnError = error.request.status === 0 || error.request.status === 500
+
+  if (canThrowAnError) {
+    throw new Error(error.message)
+  }
+
+  return error
+})
+
+export default {
+
+}
