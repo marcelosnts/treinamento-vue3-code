65f65c1e0c5d78b339cb80b5114baab81cf30cc2
Marcelo <santosmarcelo1995@live.com>
2021-05-16 13:45:23 -0300

    Add emits prop

--- widget/src/views/Widget/Standby.vue
+++ widget/src/views/Widget/Standby.vue
@@ -28,6 +28,7 @@ interface SetupReturn {
 
 export default defineComponent({
   components: { Icon },
+  emits: ['open-box'],
   setup (_, { emit }: SetupContext): SetupReturn {
     return { emit }
   }
