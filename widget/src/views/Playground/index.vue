c7239fb325b6f8fbbc0c612ad0e335e2aa0e8bb3
Marcelo <santosmarcelo1995@live.com>
2021-05-16 11:42:31 -0300

    Env config

---
+++ widget/src/views/Playground/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-main">
+    <h1 class="text-6xl font-black text-brand-gray">Playground</h1>
+    <p class="text-2xl mt-3 font-regular text-brand-gray">This the playground for widget testing!</p>
+  </div>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-gray">
+    <h1 class="text-6xl font-black text-brand-graydark">Section 2</h1>
+  </div>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-main">
+    <h1 class="text-6xl font-black text-brand-gray">Section 3</h1>
+  </div>
+  <div class="w-full h-3/4 flex flex-col justify-center items-center bg-brand-gray">
+    <h1 class="text-6xl font-black text-brand-graydark">Section 4</h1>
+  </div>
+  <widget />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Widget from '../Widget/index.vue'
+
+export default defineComponent({
+  components: { Widget }
+})
+</script>
